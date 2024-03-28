4f09c3740d45df6bc4b24eee7bee99fa4681fdf4
HoeSeong123 <ghltjd3@gmail.com>
2024-03-28 11:06:50 +0900

    feat(OutputView) : 게임 시작 문구 변경

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -18,6 +18,7 @@ public class OutputView {
             }
             count++;
         }
+        System.out.println();
     }
 
     private static void printPiece(Piece piece) {
@@ -25,8 +26,10 @@ public class OutputView {
     }
 
     public static void printStartMessage() {
-        System.out.println("체스 게임을 시작합니다.");
-        System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
+        System.out.println("> 체스 게임을 시작합니다.");
+        System.out.println("> 게임 시작 : start");
+        System.out.println("> 게임 종료 : end");
+        System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3`");
     }
 
     private static EnumMap<Role, String> initializePiece() {
