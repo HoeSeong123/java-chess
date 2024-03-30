1d6b1aafc7ed4aa7a5f51fc116d748a577f06150
HoeSeong123 <ghltjd3@gmail.com>
2024-03-30 09:49:44 +0900

    refactor(RetryUtil) : 생성자 제한

--- src/main/java/chess/util/RetryUtil.java
+++ src/main/java/chess/util/RetryUtil.java
@@ -4,6 +4,10 @@ import chess.view.OutputView;
 import java.util.function.Supplier;
 
 public class RetryUtil {
+
+    private RetryUtil() {
+    }
+
     public static <T> T read(Supplier<T> supplier) {
         try {
             return supplier.get();
